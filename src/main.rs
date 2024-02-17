e747ab5ee8a1a4f445b436dd1e360043944106d2
KSY <sykim.p508@gmail.com>
2024-02-17 18:19:33 +0900

    Update main.rs: check dir and create .db file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rusqlite::Connection;
+use std::{fs, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let path = define_path();
+    let path = check_dir(&path);
+    check_db(&path);
+}
+
+fn define_path() -> PathBuf {
+    dirs::home_dir().unwrap().join(".dawn")
+}
+
+fn check_dir(path: &PathBuf) -> PathBuf {
+    if !&path.exists() {
+        fs::create_dir(&path).unwrap();
+    }
+    path.join("dawn.db")
+}
+
+fn check_db(path: &PathBuf) {
+    let _conn = Connection::open(&path).unwrap();
+    // TODO: Check if the DB schema is fine
+    /*
+    conn.pragma_query_value("dawn", "user_version", |row| row.get(0)).unwrap();
+    conn.execute_batch(include_str!("./sql/create_schema.sql")).expect("");
+    */
 }
