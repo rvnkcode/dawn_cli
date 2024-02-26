b9851f9f2ba0f808ae3836d35c868ba99cec433c
rvnkcode <sykim.p508@gmail.com>
2024-02-26 14:11:32 +0900

    Added test sql

---
+++ src/sql/test.sql
@@ -0,0 +1,9 @@
+/*
+INSERT INTO task (id, title) VALUES ('012345678912345', 'test');
+
+SELECT * FROM task;
+
+INSERT INTO task (id, title) VALUES ('012345678912346', 'test');
+
+UPDATE task SET title = 'updated' WHERE id IS NOT NULL;
+*/
