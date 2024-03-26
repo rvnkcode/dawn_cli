3f1779f93da639b105cea084457d1ab052de4c06
KSY <sykim.p508@gmail.com>
2024-03-26 21:47:54 +0900

    Updated create_schema.sql: added columns to the task table

--- src/sql/create_schema.sql
+++ src/sql/create_schema.sql
@@ -1,9 +1,17 @@
 -- From user_version 0 to 1
 CREATE TABLE IF NOT EXISTS task (
     id TEXT PRIMARY KEY CHECK (length(id) = 15),
+    -- Got lint parser error but works fine
+    priority TEXT CHECK (priority GLOB '[A-Z]'), 
     title TEXT NOT NULL,
+    note TEXT,
+    planned_date DATE,
+    due_date DATE,
     created_at DATETIME NOT NULL DEFAULT current_timestamp,
-    updated_at DATETIME NOT NULL DEFAULT current_timestamp
+    started_at DATETIME,
+    completed_at DATETIME,
+    updated_at DATETIME NOT NULL DEFAULT current_timestamp,
+    deleted_at DATETIME
 );
 
 CREATE TRIGGER IF NOT EXISTS updated_at_task AFTER UPDATE ON task
