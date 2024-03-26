a56c7ad60da443abbc076ccf93247d7fdbd5e42f
KSY <sykim.p508@gmail.com>
2024-03-26 21:39:40 +0900

    Updated test sql

--- src/sql/test.sql
+++ src/sql/test.sql
@@ -1,9 +1,12 @@
 /*
 INSERT INTO task (id, title) VALUES ('012345678912345', 'test');
+INSERT INTO task (id, title, priority) VALUES ('012345678912345', 'test', 'a');
 
 SELECT * FROM task;
 
 INSERT INTO task (id, title) VALUES ('012345678912346', 'test');
 
 UPDATE task SET title = 'updated' WHERE id IS NOT NULL;
+
+PRAGMA table_info(task);
 */
